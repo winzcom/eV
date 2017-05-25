2fd82d520f7cce49ad2db68a3185c7e124e25d19
EC2 Default User <ec2-user@ip-172-31-26-169.eu-west-1.compute.internal>
2017-05-25 14:23:10 +0000

    from server

---
+++ public/jss/custom/data/requestdata.js
@@ -0,0 +1,186 @@
+    var MargueeAditional = [
+                            
+                            {name:'Generator Hire',value:'Generator Hire',type:'checkbox',formname:'extra[]',label:'Do you need this additional service'},
+                            {name:'Mobile Toilet',value:'Mobile Toilet',type:'checkbox',formname:'extra[]'},
+                            //{name:'Air Conditioning',value:'Air Conditioning',type:'checkbox',formname:'extra[]'},
+                            {name:'Tables and Chairs',value:'Tables and Chairs',type:'checkbox',formname:'extra[]'}
+    ]
+
+
+
+var MargueeExtras = [
+                            
+                            {name:'Style of Party',formname:'party_style',type:'select',children:[
+                              {name:'Banquet Party(e.g seat with round table)',value:'Banquet Party'},
+                              {name:'Theatrical party(e.g seat with no table)', value:'Theatrical party'}
+                            ]},
+                            {name:'Tent Flooring',formname:'tentflooring',type:'select',children:[
+                              {name:'No ',value:'No ground flooring'},
+                              {name:'Yes',value:'Yes want flooring'},
+                            ]},
+                            {
+                              name:'Do you need tent liner or pole cover',formname:'tent_liner_or_pole_cover',type:'select',children:[
+                                {name:'Tent liner',value:'Tent Liner'},
+                                {name:'Pole Cover',value:'Pole Cover'},
+                                {name:'Tent Liner and Pole Cover',value:'Tent Liner and Pole Cover'},
+                                {name:'Undecided',value:'Undecided'},
+                                {name:'None',value:'None'}
+                              ]
+                            }
+]
+
+var CateringAdditionals = [
+                            
+                            //{name:'Catering Equipment',value:'Catering Equipment',type:'checkbox',formname:'extra[]',label:'Do you need this additional service'},
+                            {name:'TableWare',value:'Catering Equipment',type:'checkbox',formname:'extra[]'},
+                            {name:'Waiting Staffs',value:'Catering Equipment',type:'checkbox',formname:'extra[]'},
+                            {name:'Bar Service',value:'Bar Service',type:'checkbox',formname:'extra[]'},
+                            //{name:'Rental/Setup',value:'Rental/Setup',type:'checkbox',formname:'extra[]'},
+                            //{name:'Chair Covers',value:'Chair Covers',type:'checkbox',formname:'extra[]'}
+                            
+]
+
+var SmallChopsAdditional = [
+                             {name:'Spring Rolls',value:'Sprinkle',type:'checkbox',formname:'extra[]',label:'What do you want in your small chops'},
+                             {name:'Samosa',value:'Samosa',type:'checkbox',formname:'extra[]'},
+                             {name:'Gizzard',value:'Gizzard',type:'checkbox',formname:'extra[]'},
+                             {name:'Fish',value:'Fish',type:'checkbox',formname:'extra[]'},
+                             {name:'Chicken',value:'Chicken',type:'checkbox',formname:'extra[]'},
+                             {name:'Meat',value:'Mosa',type:'checkbox',formname:'extra[]'},
+                             {name:'Puff Puff',value:'Puff Puff',type:'checkbox',formname:'extra[]'},
+                             {name:'Beer Battered Fish',value:'Beer Battered Fish',type:'checkbox',formname:'extra[]'}
+                            // {name:'tell us more',type:'text',formname:'iwantthisincludedalsointhesmallchops',value:''}
+]
+
+var eventPlannerAdditional = [
+                            {name:'MC',value:'MC',type:'checkbox',formname:'extra[]',label:'Do you need this additional service'},
+                            {name:'Catering',value:'Catering',type:'checkbox',formname:'extra[]'},
+                            {name:'Photography',value:'Photography',type:'checkbox',formname:'extra[]'},
+                            {name:'Video Coverage',value:'Video Coverage',type:'checkbox',formname:'extra[]'},
+                            {name:'Music/Bands',value:'Music/Bands',type:'checkbox',formname:'extra[]'},
+                            {name:'Outfits',value:'Outfits',type:'checkbox',formname:'extra[]'}
+]
+
+var placeholder = `please tell us about the type of meal, dietery requirement we need to know about if any, 
+                   do you want the food cooked before the event and stored in a warm container or cooked on site
+                   tell us about water access any catering equipment you want
+                                 `
+
+
+
+var CateringExtras = [
+                        //{name:'Dietary requirement',value:'',type:'text',formname:'dietary_requirement'},
+                        //{name:'Allergies we need to know about',value:'',type:'text',formname:'allergies'}
+]
+
+/*var CakeExtras = [
+                    {
+                      name:'Flavours',formname:'cake_flavours',type:'select',children:[
+                        {name:'Sugar',value:'Sugar'},
+                        {name:'Chocolate',value:'Chocolate'},
+                        {name:'Cocoa',value:'Cocoa'},
+                        {name:'Butter',value:'Butter'},
+                        {name:'Vanilla',value:'Vanilla'}
+                      ]
+                    }
+]*/
+
+var CakeExtras = [
+                    {name:'Sugar',value:'Sugar',type:'checkbox',formname:'flavours[]',label:'Please select your flavour from the list'},
+                    {name:'Chocolate',value:'Chocolate',type:'checkbox',formname:'flavours[]'},
+                    {name:'Vanilla',value:'Vanilla',type:'checkbox',formname:'flavours[]'},
+                    {name:'Butter',value:'Butter',type:'checkbox',formname:'flavours[]'},
+                    {name:'Cocoa',value:'Cocoa',type:'checkbox',formname:'flavours[]'},
+                    {name:'Other(please type below other flavours you want)',value:'Other',type:'text',formname:'flavours_i_want_included'}
+        
+]
+
+var publicPrivateEvents = [{name:'Public Event',type:'radio',formname:'eventtype',siblings:
+
+                          [{name:'Name of Event',formname:'eventname'},{name:'Website',formname:'eventwebsite'}]                          
+                      },
+                      {name:'Private Event',type:'radio',formname:'eventtype'}]
+  
+var eventPlanning = ['EventPlanner','WeddingPlanner']; var cateringPlaceholder = 'Please tell us more about your needs e.g your main meals, side meals what type of drinks you want etc'; var placeholder = 'Please tell us more about your events, extra needs and services';
+
+    var formElements = {
+      'Caterers':{
+                    'additional':CateringAdditionals,
+                     extras:CateringExtras,
+                     placeholder:cateringPlaceholder 
+                },
+      'SmallChops':{
+                      'additional':SmallChopsAdditional,
+                      placeholder:placeholder
+      },
+      'Canapes':{
+                     'additional':CateringAdditionals,
+                      extras:CateringExtras,
+                      placeholder:placeholder 
+                  },
+      'VegetarianandVeganCatering':{
+                     'additional':CateringAdditionals,
+                      extras:CateringExtras,
+                      placeholder:placeholder 
+                  },
+      'TableWare':{
+                     'additional':CateringAdditionals
+                  },
+      'CorporateEventCatering':{
+                     'additional':CateringAdditionals,
+                      extras:CateringExtras,
+                      placeholder:placeholder
+                  },
+      'Buffet':{
+                     'additional':CateringAdditionals,
+                      extras:CateringExtras,
+                      placeholder:placeholder
+                  },
+      'CakeMakers':{
+                      'extras':CakeExtras
+                  },
+      'BellTents': {
+                          'additional':MargueeAditional,
+                          'extras':MargueeExtras
+                  },
+        'EventPlanner':{
+                          'additional':CateringAdditionals.concat(eventPlannerAdditional),
+                          'extras':MargueeExtras
+        },
+       'BusinessLunchCatering':{
+                                      'additional':CateringAdditionals,
+                                       'extras':CateringExtras,
+                                       placeholder:placeholder
+                                },
+      'PartyTents':{
+                        'additional':MargueeAditional,
+                          'extras':MargueeExtras
+                    },
+        'TipiHire':{
+                          'additional':MargueeAditional,
+                          'extras':MargueeExtras
+                   },
+        'Transport':{
+
+                        'extras':[
+
+                                {
+                                  name:'Pick-up Location',formname:'pickuplocation',type:'text'
+                                },
+                                {
+                                  name:'Destination',formname:'destination', type:'text'
+                                }
+                        ]
+
+                    },
+          'Event Planner':{
+
+          }
+    }
+
+
+
+    var eventType = {
+      'Business Event':publicPrivateEvents,
+    }
+
---
+++ public/jss/custom/startRequestwizard.js
@@ -0,0 +1,492 @@
+$(document).ready(function() {
+
+
+        /*if (document.getElementById('user_state').length > 0) {
+            var user_state = document.getElementById('user_state').dataset.userState;
+            //alert(user_state);
+        }*/
+
+        
+
+
+        try{
+            $( "#datepicker" ).datepicker({
+                minDate:new Date(new Date().setDate(new Date().getDate()))
+            });
+        }catch(err){
+
+        }
+
+        var request_url = $('#myWizard').attr('action');
+        var button;
+        var v_available = false;
+        statee = null;
+
+        $('#state').change(function() {
+
+            v_available = true;
+            statee = $(this).val();
+        })
+
+        $('#myModal').modalSteps({
+            btnCancelHtml: 'Cancel',
+            btnPreviousHtml: 'Previous',
+            btnNextHtml: 'Next',
+            btnLastStepHtml: 'Complete',
+            completeCallback: function() {
+                /*** Ajax Call To Submit Form **/
+
+                //console.log($('#myWizard').serialize())
+                alertify.delay(0).log("Request is been sent...").maxLogItems(1);
+
+                //var formData = $('#myWizard').serializeArray().reduce(function(a, x) { a[x.name] = x.value; return a; }, {});
+                var formData = $('#myWizard').serialize();
+                console.log(formData);
+
+                $.ajax({
+                    url: request_url,
+                    type: 'POST',
+                    data: formData,
+                    headers: {
+                        'X-CSRF-TOKEN': Laravel.csrfToken,
+                    },
+                    success: function(data) {
+                        console.log(data);
+                        try {
+
+                            var d = JSON.parse(data);
+                            if (d.error) {
+                                var html = "<p style='color:white'>Error: " + d.error + "</p>"
+                                alertify.closeLogOnClick(true).error(html, function(ev) {
+                                    $('#myModal').modal('show');
+                                });
+                            } else {
+                                var html = "<p style='color:white'>Error: " + d.message + "</p>"
+                                alertify.closeLogOnClick(true).sucess(html);
+                            }
+                        } catch (e) {
+                            alertify.success('Request Sent');
+                        }
+
+                    },
+                    error: function(err) {
+                        console.log(err.error);
+                        alertify.log(err.error);
+                    }
+                })
+            }
+        }); /***End of Modal Step */
+
+
+
+        $('#myModal').on('shown.bs.modal', function(event) {
+
+            var modal = $(this);
+            button = $(event.relatedTarget);
+
+            if (button.data('state') !== undefined && button.data('state') !== '') {
+            	
+                var state = button.data('state');
+                var vicinity_id = null;
+                if (button.data('vicinity') !== '' && button.data('vicinity') !== undefined) vicinity_id = button.data('vicinity');
+                else vicinity_id = 0;
+                 $("#category").trigger("change");
+                 //checkVendorAvailability($('#category').val(),state,vicinity_id);
+                addStateLocality(state, vicinity_id);
+            } else {
+                if ($('#category').val() == '') {
+                    disableNextButton(true);
+                }else if($('#category').val() !== '' && v_available == true){
+                    disableNextButton(true);
+                    checkVendorAvailability($('#category').val(),$('#state').val(),$('#vicinity').val())
+                }
+                addStateLocality();
+            }
+            //var cate = $('#category');
+            //var c = $('#category option:selected').text().replace('- ','');
+
+
+            $('#venue').show();
+            toggleBudgetFields(false);
+        })
+
+        /***Start of Category event Operation */
+        $('#category').on('change', function() {
+
+            //disableNextButton();
+            $('.themecolor').remove()
+            categoryChange($(this));
+        }) /***End of Category event Operation */
+
+
+        /*** Start of event Change operation */
+
+        $('#event').change(function() {
+            var self = $(this);
+
+            //$('.themecolor').remove();
+            $('.pP').remove();
+
+            var event = $('#event option:selected').text();
+
+            var exists = eventType[event] || null;
+
+            //var category = $('#category option:selected').text().replace(/\s+/g, '');
+
+            if (exists !== null) {
+                //addExtraFormElement(exists, self)
+            }
+        }) /***End of event Change Event operation */
+
+        $('#browsevendor').change(function() {
+            var cat = $(this).val();
+            if (!isNaN(cat))
+                window.location.href = myUrl + 'browse_vendors/' + cat;
+        })
+
+
+        $('#start_request').submit(function() {
+
+            //display the form wizard;
+            
+            return false;
+
+        }); /***End of Jquery events changeOperation */
+
+        /***Start of vanilla functions */
+
+        function disableNextButton(disable = false) {
+            d_orientations = $('.js-btn-step');
+            d_orientations.each(function(i, v) {
+                if (v.dataset.orientation == 'next')
+                    v.disabled = disable;
+            })
+        }
+
+
+
+        function checkVendorAvailability(cat_id, state = null, locality = null) {
+
+            if (state == null || locality == null) {
+                state = $('#start').data('state') || $('#state').val();
+                locality = $('#start').data('vicinity') || $('#vicinity').val() || 0;
+                if (locality == 'all')
+                	locality = 0;
+            }
+            var vendor_available_span = $('.vendor_available');
+            var data = { 'state': state, 'locality': locality, 'category': cat_id };
+            console.log(data);
+            vendor_available_span.css('color', 'blue');
+            vendor_available_span.text('Checking vendors available...')
+                //alertify.log("Checking if vendors are available").maxLogItems(1);
+
+            $.ajax({
+                url: myUrl + 'check_vendor_availabity',
+                type: 'GET',
+                dataType: 'json',
+                data: data,
+                success: function(data) {
+                    if (data.available == 0) {
+                        console.log(data.available)
+                        v_available = false;
+                        vendor_available_span.css('color', 'red')
+                        vendor_available_span.text('no vendors available')
+                        disableNextButton(true);
+                    } else {
+
+                        vendor_available_span.css('color', 'green')
+                        vendor_available_span.text(data.available + ' vendor(s) available')
+                        disableNextButton();
+                    }
+                }
+            })
+        }
+
+        function categoryChange(self) {
+
+            if ($('.divContainer')) {
+                $('.divContainer').remove();
+            }
+
+            var category = $('#category option:selected').text();
+            category = category.replace(/\s+/g, '');
+
+
+            var state = $('#state').val();
+            var locality = $('#vicinity').val();
+            checkVendorAvailability(self.val(), state, locality)
+
+
+
+            console.log(category)
+
+            var data = formElements[category];
+            
+            console.log(data);
+            if (data) {
+            	if(data.placeholder !== undefined){
+            		$('#personalmessage').attr('placeholder',data.placeholder);
+            	}
+                addAdditionalService(data, self,category);
+            } else if ($.inArray(category, eventPlanning) != -1) {
+                //addThemeColorInputElements($('#div_event'));
+            } else if (category == 'Transport') {
+                addTransportInputs();
+                $('#venue').hide();
+            }
+
+
+
+        }
+
+        function addStateLocality(state1 = null, vicinity_id = null) {
+
+            var state, locality, stateInput, localityInput, inputs;
+
+            if (state1 !== null && vicinity_id !== null) {
+
+                console.log(vicinity_id + ' ' + state1);
+                if (vicinity_id== 'all')
+                    vicinity_id= 0;
+
+                stateInput = $('<input type="hidden" name="state" value="' + state1 + '" ></input>');
+                localityInput = $('<input type="hidden" name="vicinity" value="' + vicinity_id+ '"></input>');
+                $('#myWizard').append(stateInput);
+                $('#myWizard').append(localityInput);
+            } else {
+
+                state = $('#state').val()
+                locality = $('#vicinity').val();
+
+                if (state == undefined || state == '') {
+
+                    $('#myModal').modal('hide');
+                    alertify.alert('Please select a state and locality')
+
+                } else {
+
+                    if (locality == 'all')
+                        locality = 0;
+                    stateInput = $('<input type="hidden" name="state" value="' + state + '" ></input>');
+                    localityInput = $('<input type="hidden" name="vicinity" value="' + locality + '"></input>');
+                    $('#myWizard').append(stateInput);
+                    $('#myWizard').append(localityInput);
+                }
+            }
+        }
+
+
+        function addAdditionalService(data, ele,category) {
+
+            var divContainer = $('<div class="control-group divContainer" style="padding-left:10px;"></div>');
+
+            if(data.additional !== undefined){
+
+                var p = $('<p style="text-align:center;">Do you want this additional service(s)</p>');
+                if(category == 'SmallChops'){
+                    p.text('What do you want in the smallchops');
+                }
+                divContainer.html(p);
+                var outerDiv = $('<div class="row">');
+                 data.additional.forEach(function(element) {
+
+                
+                var divContent = $(`<div class="checkbox col-md-3 col-xs-6"></div>`);
+                var outerLabel = $('<label>');
+                var className = element.type === 'text' ? '':'';
+                var formInput = $('<input type="'+element.type+'" name="'+element.formname+'"  value="' + element.value + '">')
+                        // var formInput = $('<input type="checkbox" name="extra[]" class="" value="'+element.id+'">'+element.name+'</input>')
+                    outerLabel.append(formInput)
+                    outerLabel.append(element.name)
+                    divContent.append(outerLabel)
+                    outerDiv.append(divContent);
+                    //divContent.append();
+                    
+                    
+
+                }, this);
+                divContainer.append(outerDiv);
+            }
+            
+
+            
+            
+            if (data.extras != null) {
+                var oDiv = $('<div class="row">');
+                 var innerDiv = null;
+                var input = null;
+                data.extras.forEach(function(ele) {
+                    innerDiv = $('<div class="col-xs-6 col-md-4">')
+                    if(ele.type === 'text'){
+                         input = $('<input>');
+                         input.attr({
+                            name: ele.formname,
+                            class: 'form-control input-sm',
+                            value: ''
+                         }, this);
+                         var label = $('<label>')
+                         label.text(ele.name);
+                         innerDiv.append(label)
+                         innerDiv.append(input);
+                         oDiv.append(innerDiv);
+                    } else if(ele.type === 'select' && ele.children.length > 0){
+                        input = $('<select class="form-control input-sm" name="'+ele.formname+'">');
+                        ele.children.forEach(function(e){
+                            var option = $('<option>');
+                            option.attr({
+                                value:e.value
+                            })
+                            option.text(e.name);
+                            input.append(option);
+                        })
+                        var label = $('<label>')
+                        label.text(ele.name);
+                        innerDiv.append(label);
+
+                        innerDiv.append(input);
+                        oDiv.append(innerDiv);
+                    }else{
+                        input = $('<input>');
+                         input.attr({
+                            name: ele.formname,
+                            class: 'form-control input-sm',
+                            value: ele.value,
+                            type:ele.type
+                         }, this);
+                         var label = $('<label>')
+                         label.text(ele.name);
+                         innerDiv.append(label)
+                         innerDiv.append(input);
+                         oDiv.append(innerDiv);
+                    }
+                    
+                })
+                divContainer.append(oDiv);
+            }
+            if(data.placeholder !== ''){
+                var pmtextarea = $('#personalmessage');
+                pmtextarea.attr('placeholder',data.placeholder);
+            }
+            divContainer.insertAfter(ele)
+        }
+
+        function addThemeColorInputElements(element) {
+            var e = element;
+
+            var divContainer = $(`<div class="control-group themecolor">
+                              <label class="control-label" for="inputCity">Please Select Theme and Color</label></br>
+                             </div>
+                          `);
+
+            var themeQuestionSelect = $('<select name="theme"></select></br>');
+            themeQuestionSelect.addClass('form-control input-lg');
+            var themeLabel = $('<label class="control-label">Theme</label></br>');
+
+
+            divContainer.append(themeLabel)
+            var options = ['Beach', 'Sand'];
+
+            options.forEach(function(element) {
+                var opt = $('<option value="' + element + '">' + element + '</option>');
+                themeQuestionSelect.append(opt);
+            }, this);
+
+            divContainer.append(themeQuestionSelect);
+
+            var colorQuestionInput = $('<input name="color"></input>');
+            colorQuestionInput.addClass('form-control input-lg');
+            var colorLabel = $('<label class="control-label">Color</label></br>');
+            divContainer.append(colorLabel);
+
+
+            divContainer.append(colorQuestionInput);
+
+            $('#eventtype').append(divContainer);
+            // divContainer.insertAfter(e);
+
+        }
+
+        function addExtraFormElement(e, element) {
+            var divC = $('<div>');
+            divC.addClass('control-group pP');
+
+            var label = $('<label>');
+            var br = $('<br>');
+            label.text('Is it a Public or Private Event');
+
+
+
+
+            divC.append(label);
+            br.insertAfter(label);
+
+            e.forEach(function(element) {
+
+                var pue = $('<input>')
+
+                pue.attr({
+                    name: element.formname,
+                    type: element.type,
+                    value: element.name,
+                    class: 'form-control'
+                });
+
+                pue.on('click', function() {
+
+                    if (element.hasOwnProperty('siblings')) {
+
+                        public = true;
+                        toggleBudgetFields(public);
+
+                        element.siblings.forEach(function(e) {
+
+                            var sibling = $('<input>');
+                            var label = $('<label>');
+
+                            label.attr('class', 'os');
+                            label.text(e.name);
+
+                            sibling.attr({
+                                name: e.formname,
+                                class: 'form-control os',
+                            })
+
+                            if (e.name != 'Website')
+                                sibling.attr('required', true);
+
+                            divC.append(label);
+                            sibling.insertAfter(label);
+                        }, this)
+                    } else {
+                        $('.os').remove();
+                        public = false;
+                        toggleBudgetFields(public)
+                    }
+                })
+
+                var pLabels = $('<label>');
+                pLabels.text(element.name);
+
+                divC.append(pLabels);
+
+                divC.append(pue);
+            })
+            $('#eventtype').append(divC);
+            //divC.insertAfter(next);
+        }
+
+        function toggleBudgetFields(public) {
+            if (public) {
+                $('#normalbudget').hide();
+                $('#publicbudget').show();
+            } else {
+                $('#publicbudget').hide();
+                $('#normalbudget').show();
+            }
+        }
+
+        function addTransportInputs() {}
+
+
+
+    }) //doument.ready
